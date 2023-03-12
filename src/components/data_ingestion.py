4b3fce6cb55a421b0fa6c93ee6d72506c5a2b525
javeedmech <javeedmech@gmail.com>
2023-03-12 12:16:13 +0530

    end of classs 4

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -37,7 +37,7 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Inmgestion of the data iss completed")
+            logging.info("Inmgestion of the data is completed")
 
             return(
                 self.ingestion_config.train_data_path,
@@ -49,10 +49,12 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
+    obj.initiate_data_ingestion
 
-    data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    # train_data,test_data=obj.initiate_data_ingestion()
+
+    # data_transformation=DataTransformation()
+    # data_transformation.initiate_data_transformation(train_data,test_data)
 
 
 
